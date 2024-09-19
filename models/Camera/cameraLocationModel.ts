baf0bdea7f21378c0fe197d40e18b7d23e335de6
Christine <christine.helfrich1@gmail.com>
2024-09-19 08:45:59 -0700

    add models

--- models/Camera/cameraLocationModel.ts
+++ models/Camera/cameraLocationModel.ts
@@ -1,7 +1,104 @@
-export interface CameraLocationModel {
+export interface CameraLocationResponseModel {
+    code: number,
+    data: CameraLocationResponseDataModel,
+    err: string
+    rendered: string,
+    str: string,
+}
+
+export interface CameraLocationResponseDataModel {
+    cams: CameraLocationResponseDataCamsModel,
+    locs: CameraLocationResponseDataLocsModel,
+}
+
+export interface CameraLocationResponseDataLocsModel {
+    data: CameraLocationResponseDataLocsDataModel[],
+    key: CameraLocationResponseDataLocsKeyModel
+}
+
+export interface CameraLocationResponseDataCamsModel {
+    data: CameraLocationResponseDataCamsDataModel[],
+    key: CameraLocationResponseDataCamsKeyModel
+}
+
+export interface CameraLocationResponseDataLocsDataModel {
     id: number,
     lat: number,
     lon: Float32Array,
     st: string,
     lp: number
+}
+
+export interface CameraLocationResponseDataLocsKeyModel {
+    id: string,
+    lat: string,
+    lon: string,
+    st: string,
+    lp: string
+}
+
+export interface CameraLocationResponseDataCamsKeyModel {
+    af: string,
+    ato: string,
+    br: string,
+    cc: string,
+    cl: string,
+    cn: string,
+    co: string, 
+    foc: string,
+    fov: string, 
+    hn: string,
+    id: string,
+    img: string, 
+    isp: string, 
+    lid: string, 
+    lmt: string, 
+    off: string,
+    p: string,
+    pi: string,
+    pl: string,
+    pr: string,
+    ps: string,
+    ptz: string,
+    pv: string,
+    sp: string,
+    st: string,
+    t: string,
+    tb: string,
+    tr: string,
+    trg: string, 
+    typ: string,
+}
+
+export interface CameraLocationResponseDataCamsDataModel {
+    af: number,
+    ato: number,
+    br: string,
+    cc: string,
+    cl: number,
+    cn: string,
+    co: string, 
+    foc: string,
+    fov: string, 
+    hn: string,
+    id: string,
+    img: string, 
+    isp: string, 
+    lid: number, 
+    lmt: string, 
+    off: number,
+    p: string,
+    pi: string,
+    pl: string,
+    pr: string,
+    ps: string,
+    ptz: number,
+    pv: number,
+    sp: string,
+    st: string,
+    t: string,
+    tb: number,
+    tr: string,
+    trg: number, 
+    typ: string,
 }
--- services/Cameras/CameraService.ts
+++ services/Cameras/CameraService.ts
@@ -1,4 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 export const GetCameraLocations = async () => {
+      // Fetch data from external API
+    //const res = await fetch('https://api.cdn.prod.alertwest.com/api/getCameraDataByLoc')
+    //const repo: any = await res.json()
+    // Pass data to the page via props
+    // console.log(repo)
+    // return repo.data.locs.data
     return [
         {longitude: -117.24788118125575, latitude: 32.94823280197382}, 
         {longitude: -117.16020678817303, latitude: 32.73760419615398},
